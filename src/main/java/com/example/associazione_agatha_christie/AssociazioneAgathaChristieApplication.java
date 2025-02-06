aa73d2c1735676954be19328d0c059bafe268017
Twalian <napolitizy@gmail.com>
2025-02-06 15:29:16 +0100

    Creazione progetto

---
+++ src/main/java/com/example/associazione_agatha_christie/AssociazioneAgathaChristieApplication.java
@@ -0,0 +1,13 @@
+package com.example.associazione_agatha_christie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssociazioneAgathaChristieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssociazioneAgathaChristieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/associazione_agatha_christie/AssociazioneAgathaChristieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.associazione_agatha_christie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssociazioneAgathaChristieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
