590fa3895bc1bc29d36e6339ea43f6bfea75499f
Supermanu96 <elafini96@outlook.it>
2025-02-06 17:12:44 +0100

    Model ciclo credenziale

---
+++ src/main/java/com/example/associazione_agatha_christie/model/Credenziale.java
@@ -0,0 +1,56 @@
+package com.example.associazione_agatha_christie.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
+
+@Entity
+@Table(name = "credenziali")
+public class Credenziale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    @Pattern(regexp = "[a-zA-Z0-9._-]{1,50}", message = "Caratteri Non Ammessi")
+    private String username;
+
+    @Column
+    @Pattern(regexp = "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,50}", message = "Password Troppo Debole")
+    private String password;
+
+    @Column
+    private String ruolo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRuolo() {
+        return ruolo;
+    }
+
+    public void setRuolo(String ruolo) {
+        this.ruolo = ruolo;
+    }
+}
