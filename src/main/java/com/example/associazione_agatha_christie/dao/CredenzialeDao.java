008ba64749e3022144389e66113e6d61a35e1f3a
Twalian <napolitizy@gmail.com>
2025-02-07 10:50:37 +0100

    Create CredenzialeDao.java

---
+++ src/main/java/com/example/associazione_agatha_christie/dao/CredenzialeDao.java
@@ -0,0 +1,7 @@
+package com.example.associazione_agatha_christie.dao;
+
+import com.example.associazione_agatha_christie.model.Credenziale;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CredenzialeDao extends CrudRepository<Credenziale, Integer> {
+}
