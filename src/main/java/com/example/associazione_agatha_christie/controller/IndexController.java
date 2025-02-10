ff99753c0b5d552c7ec90f2fa9e6b05e9767b003
Twalian <napolitizy@gmail.com>
2025-02-10 15:31:25 +0100

    Collegamento css, carosello, index controller (libri)

--- src/main/java/com/example/associazione_agatha_christie/controller/IndexController.java
+++ src/main/java/com/example/associazione_agatha_christie/controller/IndexController.java
@@ -1,15 +1,28 @@
 package com.example.associazione_agatha_christie.controller;
 
+import com.example.associazione_agatha_christie.model.Libro;
+import com.example.associazione_agatha_christie.service.LibroService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
 
+    @Autowired
+    private LibroService libroService;
+
     @GetMapping
-    public String getPage() {
-        return "index";
+    public String getPage(Model model) {
+
+        List<Libro> libri = libroService.elencoLibri();
+        model.addAttribute("libri", libri);
+
+        return "index1";
     }
 }
