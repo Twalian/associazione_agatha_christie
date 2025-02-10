8e5e38229fe730c313285a9835a217418e5e1102
Irene0398 <irenebarreca2@gmail.com>
2025-02-10 09:58:10 +0100

    eventiController, libriController

--- src/main/java/com/example/associazione_agatha_christie/controller/EventiController.java
+++ src/main/java/com/example/associazione_agatha_christie/controller/EventiController.java
@@ -1,15 +1,26 @@
 package com.example.associazione_agatha_christie.controller;
 
+import com.example.associazione_agatha_christie.model.Evento;
+import com.example.associazione_agatha_christie.service.EventoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/eventi")
 public class EventiController {
 
+    @Autowired
+    private EventoService eventoService;
+
     @GetMapping
-    public String getPage() {
+    public String getPage(Model model) {
+        List<Evento> eventi = eventoService.elencoEventi();
+        model.addAttribute("eventi", eventi);
         return "eventi";
     }
 }
--- src/main/java/com/example/associazione_agatha_christie/controller/LibriController.java
+++ src/main/java/com/example/associazione_agatha_christie/controller/LibriController.java
@@ -2,16 +2,27 @@ package com.example.associazione_agatha_christie.controller;
 
 
 
+import com.example.associazione_agatha_christie.model.Libro;
+import com.example.associazione_agatha_christie.service.LibroService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/libri")
 public class LibriController {
 
+    @Autowired
+    private LibroService libroService;
+
     @GetMapping
-    public String getpage() {
+    public String getpage(Model model) {
+        List<Libro> libri = libroService.elencoLibri();
+        model.addAttribute("libri", libri);
         return "libri";
     }
 }
